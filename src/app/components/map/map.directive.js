7a67d6de596caec56840db300e02e3c5e7673535
NickCrawford <ncrawfo7@kent.edu>
2016-11-09 19:26:30 -0500

    Adds leaflet-draw directive

--- src/app/components/map/map.directive.js
+++ src/app/components/map/map.directive.js
@@ -24,24 +24,9 @@
       vm.height = $window.innerHeight - 40 
       - (angular.element(document.querySelector('.mdl-layout__header'))[0].offsetHeight)
       - (angular.element(document.querySelector('.mdl-mini-footer'))[0].offsetHeight);
-      console.log(vm.height);
       
-      $scope.defaults = {
-        tileLayer: "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-        zoomControlPosition: 'topright',
-        tileLayerOptions: {
-          opacity: 0.9,
-          detectRetina: true,
-          reuseTiles: true,
-        },
-        scrollWheelZoom: false
-      }
+      console.log(vm.height);
 
-      $scope.porto = {
-        lat: 41.1414,
-        lng: -8.61864,
-        zoom: 12
-      }
     }
   }
 
