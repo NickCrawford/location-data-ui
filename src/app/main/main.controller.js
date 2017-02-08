7b2c0ff4d464b47be52cb45f59e7232af41c25a4
AlexMa1989 <mc8891736@gmail.com>
2017-02-08 12:38:26 -0500

    Adds clustering

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,227 +1,218 @@
 (function(){
 
-	'use strict';
+    'use strict';
 
-	angular
-	.module('locationDataUi')
-	.controller('MainController', MainController);
+    angular
+    .module('locationDataUi')
+    .controller('MainController', MainController);
 
-	function MainController($scope, $log, leafletDrawEvents, PointQueryService) {
-		var drawnItems = new L.FeatureGroup();
+    function MainController($scope, leafletDrawEvents, MapQueryService) {
 
-		angular.extend($scope, {
-			map: {
-				porto: {
-					lat: 41.1414,
-					lng: -8.61864,
-					zoom: 16
-				},
-				drawOptions: {
-					position: "topleft",
-					draw: {
-						polyline: {
-							metric: false
-						},
-						polygon: {
-							metric: false,
-							showArea: true,
-							drawError: {
-								color: '#b00b00',
-								timeout: 1000
-							},
-							shapeOptions: {
-								color: 'blue'
-							}
-						},
-						circle: {
-							showArea: true,
-							metric: false,
-							shapeOptions: {
-								color: '#662d91'
-							}
-						},
-						marker: false
-					},
-					edit: {
-						featureGroup: drawnItems,
-						remove: true
-					}
-				}
-			},
-			startDate: new Date(Date.UTC(1970, 0, 1, 0, 0, 0)), //Normalize minus timezones
-			endDate: new Date()
-		});
+        var drawnItems = new L.FeatureGroup();
 
-		var handle = {
-			created: function(e,leafletEvent, leafletObject, model, modelName) {
-				drawnItems.addLayer(leafletEvent.layer);
-				$log.log(e, leafletEvent, leafletObject, model, modelName);
-				
-				if (leafletEvent.layerType === 'circle') {
-					$log.log($scope.startDate, $scope.endDate);
-					var start = $scope.startDate.getTime();
+        angular.extend($scope, {
+            map: {
+                tweets: {
+                    lat: 41.0814,
+                    lng: -81.5190,
+                    zoom: 12
+                },
 
-					var end = $scope.endDate.getTime();
-					
-					$log.log(start, end);
+/*              legend: {
+                    position: "bottomright",
+                    colors: [ '#FF0000', '#FF8800', '#FF0000', '#FF0000', '#FF0000'],
+                    labels: [ '1000', '500', '200', '100', '50' ]
+                },*/
 
-					PointQueryService.circleQuery(leafletEvent.layer._latlng.lat, leafletEvent.layer._latlng.lng, leafletEvent.layer._mRadius, start, end)
+                drawOptions: {
+                    position: "topleft",
+                    draw: {
+                        polyline: {
+                            metric: false
+                        },
+                        polygon: {
+                            metric: false,
+                            showArea: true,
+                            drawError: {
+                                color: '#b00b00',
+                                timeout: 1000
+                            },
+                            shapeOptions: {
+                                color: 'blue'
+                            }
+                        },
+                        circle: {
+                            showArea: true,
+                            metric: false,
+                            shapeOptions: {
+                                color: '#662d91',
+                                clickable: true
+                            }
+                        },
+                        marker: true
+                    },
+                    edit: {
+                        featureGroup: drawnItems,
+                        remove: true
+                    }
+                }
+            }
+        });
 
-					.then( function(result) {
+        function parseDate(input) {
+        return Date.parse(input)/1000; 
+        }
 
-						// for (var i = result.data.length - 1; i >= 0; i--) {
+        
 
-						// 	var coordinate = new L.LatLng(
-						// 		result.data[i].loc.coordinates[1], // Latitude
-						// 		result.data[i].loc.coordinates[0] //Longitude
-						// 		);
-							
-						// 	var marker = new L.Marker(coordinate, {});
-						// 	marker.addTo(leafletObject);
-						// }
+        var handle = {
+            created: function(e,leafletEvent, leafletObject, model, modelName) {
+                drawnItems.addLayer(leafletEvent.layer);
+                console.log(e, leafletEvent, leafletObject, model, modelName);
+                //var legend = L.control({position: 'bottomright'});
+                if (leafletEvent.layerType === 'circle') {
+                    MapQueryService.circleQuery(leafletEvent.layer._latlng.lat, leafletEvent.layer._latlng.lng, leafletEvent.layer._mRadius)
+                    .then(function(result) {
+                        //Draw Here
+                        //console.log(result);
 
-						// Get all trip ids in the resulting points
-						var uniqueTrips = result.data.map(function(point) {return point.TripID;}); // [20432]
-						
-						//remove duplicates
-					    var seen = {};
-					    var out = [];
-					    var len = uniqueTrips.length;
-					    var j = 0;
-					    for(var i = 0; i < len; i++) {
-					         var item = uniqueTrips[i];
-					         if(seen[item] !== 1) {
-					               seen[item] = 1;
-					               out[j++] = item;
-					         }
-					    }
-					    uniqueTrips = out.sort();
+                        var filterBytimeResult = [];
 
-						//iterate through each unique trip id and find all lat and longs with that trip id
-						var polyLines = [];
+                        var datefrom = parseDate(document.getElementById('datefrom').value);
+                        var dateto = parseDate(document.getElementById('dateto').value);
+                        for (var i=0; i<result.data.length; i++){
+                            var datefromdata = parseDate(result.data[i].created_at);
+                            if (dateto > datefromdata && datefrom < datefromdata)  {
+                                filterBytimeResult.push(result.data[i]);
+                            }
+                        }           
+                        //console.log(filterBytimeResult.length);
 
-						for (var i = uniqueTrips.length - 1; i >= 0; i--) {
-							var coords = result.data.filter(function(trip) {
-								return trip.TripID === uniqueTrips[i];
-							});
-							console.log("coord", coords);
+                        if (document.getElementById('datefrom').value === "" || document.getElementById('dateto').value === "")
 
-							var latlngs = coords.map(function(point) {
-								return new L.LatLng(
-									point.loc.coordinates[1], //Lat
-									point.loc.coordinates[0] //Long
-								);
-							})
-							var colors = ['purple', 'yellow', 'orange', 'brown', 'black', 'red'];
+                        {
 
-							var polyLine = new L.Polyline(latlngs, {
-							    color: colors[i%6],
-							    weight: 3,
-							    opacity: 0.5,
-							    smoothFactor: 1
-						    });
+                            /* This part is not have date filter*/
 
-							polyLines.push(polyLine);
-						}
 
-						console.log("unique", uniqueTrips);
-						console.log("poly lines",polyLines);
-						// var result = jsObjects.filter(function( obj ) {
-						//   return obj.b == 6;
-						// });
+                            var cluster = L.markerClusterGroup();
+                            var groupMarker = L.layerGroup();
 
-						/* **************************** */
 
-						// var polyLines = [];
+                            for (var i = result.data.length - 1; i >= 0; i--) {
+                            var myIcon = L.icon({
+                                iconUrl: '/images/points.png',
+                                iconSize: [10, 10],
+                                iconAnchor: [10, 10]
+                            });
 
-						// var curTrip = null;
+                            var markers = L.marker([result.data[i].loc.coordinates[1], result.data[i].loc.coordinates[0]], {icon: myIcon}).bindPopup(result.data[i].keywords.toString()).openPopup();
+                            cluster.addLayer(markers);
 
-						// var pl = [];
+                            groupMarker.addLayer(markers);
+                            //document.write(result.data[i].keywords);
+                            //console.log(cluster);
+                            }
 
-						// for (var i = result.data.length - 1; i >= 0; i--) {
-						// 	if (curTrip == null) {
-						// 		curTrip = result.data[i].TripID; 
-						// 	}
+                          if (result.data.length > 500)
+                          {
 
-						// 	if (result.data[i].TripID == curTrip) { 
-						// 		var point = new L.LatLng(
-						// 				result.data[i].loc.coordinates[1], //Lat
-						// 				result.data[i].loc.coordinates[0] //Long
-						// 			);
-						// 		pl.push( point );
-									
-								
-						// 	} else {
-						// 		//$log.log(curTrip);
-						// 		var pline = new L.Polyline(pl, {
-						// 			    color: 'red',
-						// 			    weight: 3,
-						// 			    opacity: 0.5,
-						// 			    smoothFactor: 1
-						// 		    });
-						// 		//$log.log(pline);
-						// 		polyLines.push(pline);
-						// 	    pl = [];
-						// 	    var point = new L.LatLng(
-						// 				result.data[i].loc.coordinates[1], //Lat
-						// 				result.data[i].loc.coordinates[0] //Long
-						// 			);
-						// 		pl.push( point );
-						// 	}
-						// }
+                            leafletObject.addLayer(cluster);
 
-						for (var i = polyLines.length - 1; i >= 0; i--) {
-							//$log.log(i);
-							polyLines[i].addTo(leafletObject);
-						}
+                            cluster.on('clusterclick', function (a) {
+                                // a.layer is actually a cluster
+                                //console.log(a.layer.getAllChildMarkers());
 
-						$log.log("Done Drawing");
+                                var clickCluster = a.layer.getAllChildMarkers();
+                                var markerIn = [];
+                                
+                                for (i = 0; i < clickCluster.length; i++) {
+                                    //markerIn.push(clickCluster[i]._latlng.lat);
+                                    markerIn.push(clickCluster[i]._popup._content);
+                                }
 
-					});
-				} else if (leafletEvent.layerType === "rectangle") {
-					PointQueryService.polyQuery(leafletEvent.layer._latlngs, start, end)
+                                console.log(markerIn);
+                            });
 
-					.then( function(result) {
+                          }
+                          else
+                          {
+                            leafletObject.addLayer(groupMarker);
+                          }
 
-						for (var i = result.data.length - 1; i >= 0; i--) {
 
-							var coordinate = new L.LatLng(
-								result.data[i].loc.coordinates[1], // Latitude
-								result.data[i].loc.coordinates[0] //Longitude
-								);
-							
-							var marker = new L.Marker(coordinate, {});
-							marker.addTo(leafletObject);
-						}
-					});
-				}
-			},
-			edited: function(arg) {},
-			deleted: function(arg) {
-				var layers;
-				layers = arg.layers;
-				drawnItems.removeLayer(layer);
-			},
-			drawstart: function(arg) {},
-			drawstop: function(arg) {
 
-			},
-			editstart: function(arg) {},
-			editstop: function(arg) {},
-			deletestart: function(arg) {},
-			deletestop: function(arg) {}
-		};
+                        }
 
-		var drawEvents = leafletDrawEvents.getAvailableEvents();
-		drawEvents.forEach(function(eventName){
-			$scope.$on('leafletDirectiveDraw.' + eventName, function(e, payload) {
-				//{leafletEvent, leafletObject, model, modelName} = payload
-				var leafletEvent, leafletObject, model, modelName; //destructuring not supported by chrome yet :(
-				leafletEvent = payload.leafletEvent, leafletObject = payload.leafletObject, model = payload.model,
-				modelName = payload.modelName;
-				handle[eventName.replace('draw:','')](e,leafletEvent, leafletObject, model, modelName);
-			});
-		});
-	};
+
+                        else {
+
+                            /* This part have date filter*/
+
+
+                            var cluster = L.markerClusterGroup();
+                            var groupMarker = L.layerGroup();
+
+                        for (var i = filterBytimeResult.length - 1; i >= 0; i--) {
+                            var myIcon = L.icon({
+                                iconUrl: '/images/points.png',
+                                iconSize: [10, 10],
+                                iconAnchor: [10, 10]
+                            });
+                            var markers = new L.marker([filterBytimeResult[i].loc.coordinates[1], filterBytimeResult[i].loc.coordinates[0]], {icon: myIcon});
+
+                            cluster.addLayer(markers);
+
+                            groupMarker.addLayer(markers);
+                            //document.write(result.data[i].keywords);
+                            //console.log(result.data[i].keywords);
+
+                        }
+
+                          if (filterBytimeResult.length > 500)
+                          {
+                            leafletObject.addLayer(cluster);
+                          }
+                          else
+                          {
+                            leafletObject.addLayer(groupMarker);
+
+                          }
+
+
+                    }
+
+                    
+                        
+                    });
+                }
+            },
+            edited: function(arg) {},
+            deleted: function(arg) {
+                var layers;
+                layers = arg.layers;
+                drawnItems.removeLayer(layer);
+            },
+            drawstart: function(arg) {},
+            drawstop: function(arg) {
+
+            },
+            editstart: function(arg) {},
+            editstop: function(arg) {},
+            deletestart: function(arg) {},
+            deletestop: function(arg) {}
+        };
+
+        var drawEvents = leafletDrawEvents.getAvailableEvents();
+        drawEvents.forEach(function(eventName){
+            $scope.$on('leafletDirectiveDraw.' + eventName, function(e, payload) {
+                //{leafletEvent, leafletObject, model, modelName} = payload
+                var leafletEvent, leafletObject, model, modelName; //destructuring not supported by chrome yet :(
+                leafletEvent = payload.leafletEvent, leafletObject = payload.leafletObject, model = payload.model,
+                modelName = payload.modelName;
+                handle[eventName.replace('draw:','')](e,leafletEvent, leafletObject, model, modelName);
+            });
+        });
+    };
 
 }());
